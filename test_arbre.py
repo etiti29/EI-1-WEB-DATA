80ccdaacfda8847bad70d2a9bf83320c588f8863
etienne <etienne.lafont@student-cs.fr>
2025-06-03 09:56:37 +0200

    vectorisation marche

--- test_arbre.py
+++ test_arbre.py
@@ -1,15 +1,47 @@
+import numpy as np
+import json
+
+with open("brut_data.txt", mode='r', encoding='utf-8') as file:
+        data = json.load(file)
+    # Convertir les clés en int
+data = {int(key): [value,0] for key, value in data.items()}
+
 
 #créé le vocabluaire associé à l'ensemble des tweets sous la forme d'un dictionnaire qui associe à un mot une liste contenant son identifiant et sa fréquence dans la data
 def vocabulaire(data):   
     voc={}                  
     i=0
-    for tweet in data.values()[0]:
+    for tweet in data.values():
         mots=tweet[0].split()
-        for i in range(len(mots)):
-            if mots[i] in voc.keys():
-                if mots[i] not in tweet[:i-1]:
-                    voc[mots[i]][1]+=1
+        for k in range(len(mots)):
+            if mots[k] in voc.keys():
+                if mots[k] not in tweet[:k-1]:
+                    voc[mots[k]][1]+=1
             else :
-                voc[mots[i]]=[i,1]
+                voc[mots[k]]=[i,1]
                 i+=1
     return voc
+
+voc=vocabulaire(data)
+
+def TF_IDF(mot,tweet,vocabulaire,data): #calcule le score TF IDF d'un mot dans un tweet, connaisant l'ensemble des mots pertinents à considérer ainsi que l'ensemble des tweets du corpus
+    tf=0
+    tweet_split = tweet.split()
+    for x in tweet_split:
+        if x ==mot:
+            tf+=1
+    if tf==0:
+        return 0
+    idf = np.log(len(data.keys())/vocabulaire[mot][1])
+    return (1+ np.log(tf))*idf
+
+def vectorisation(tweet,vocabulaire,data):
+    vecteur= [0 for i in range( len( vocabulaire.keys() ) ) ]
+    tweet_split=tweet.split()
+    for x in tweet_split:
+        if x in vocabulaire.keys():
+            vecteur[vocabulaire[x][0]]=TF_IDF(x,tweet,vocabulaire,data)
+    return vecteur
+
+print(vectorisation("j'aime trop les bananes",voc,data)[11])W
+print(list(voc.keys())[10])
